cc346fe93bed683259cd7d9af66c0a4de79246e9
joyboy10 <csbt1033@gmail.com>
2022-04-13 22:41:48 +0900

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import App from "./App";
 import { createRoot } from "react-dom/client";
 import { RecoilRoot } from "recoil";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <RecoilRoot>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </RecoilRoot>
 );
